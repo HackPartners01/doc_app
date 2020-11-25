f3dc1684e1981e2d1d1fbc43205d896c0b6455b5
Gautam <gautambehal20@gmail.com>
2020-11-25 13:24:20 +0530

    Constants file updated- added colors

---
+++ lib/screens/doctor_page.dart
@@ -0,0 +1,94 @@
+import 'package:doc_app/Constants.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DoctorPage extends StatelessWidget{
+
+  String avatarImageLocation = 'assets/images/avatar.png';
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          child: Material(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: kColorLightGrey
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            alignment: Alignment.topLeft,
+
+                            child: CircleAvatar(
+                              backgroundImage: AssetImage(avatarImageLocation),
+                              radius: 50.0,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(4.0),
+                                child: Text('Dr. DrfName DrlName'),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(4.0),
+                                child: Text('MBBS/ MD from FALANA'),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(4.0),
+                                child: Text('998xxxxxxx'),
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text('Available in 10 minutes', style: TextStyle(fontSize: 20),),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 16.0, right: 16.0, bottom: 16.0),
+          child: Card(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text('Alerted nearby hospitals for ambulance assistance.', style: TextStyle(fontSize: 20),),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text('Getting Responses...',),
+                )
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+}
